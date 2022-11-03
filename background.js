68d4b38d7f9d1470590acb98f6a8890d5e3e7b16
Chang <achang35@calstatela.edu>
2022-11-03 14:43:36 -0700

    Working icon with create new tab function.

--- background.js
+++ background.js
@@ -1,9 +1,18 @@
-browser.browserAction.onClicked.addListener(async () => {
-    const tabs = await browser.tabs.query({
-        active: true,
-        currentWindow: true
-    });
+console.log("background running");
 
-    browser.tabs.sendMessage(tabs[0].id, {action : "test"})
-})
+browser.browserAction.onClicked.addListener(function() {
+    browser.tabs.create({url: browser.extension.getURL('markedPages.html')});
+});
 
+//Possibly might use
+function mark() {
+    browser.tabs.query({currentWindow: true, active: true}).then((tabs) => {
+        let list = [];
+        let tab = tabs[0];
+        if (list.includes(tabs[0])) {
+            list.push(tab);
+        }
+        document.body.innerHTML = tab.url;
+        console.log(tab.url);
+    }, console.error);
+}
