3e5afac13292977a4a0d60f5877c09bf3ef3d9d4
JesseG107 <jesseygonzalez108@gmail.com>
2022-11-07 11:51:05 -0800

    Context Menu Buttons
    Created buttons within the context menu for highlighting text.

--- background.js
+++ background.js
@@ -1,9 +1,69 @@
 browser.browserAction.onClicked.addListener(async () => {
-    const tabs = await browser.tabs.query({
-        active: true,
-        currentWindow: true
-    });
+  const tabs = await browser.tabs.query({
+    active: true,
+    currentWindow: true,
+  });
 
-    browser.tabs.sendMessage(tabs[0].id, {action : "test"})
+  browser.tabs.sendMessage(tabs[0].id, { action: "test" });
+});
+
+console.log("extension loaded!");
+
+
+
+
+
+browser.menus.onClicked.addListener((info, tab) => {
+  if(info.menuItemId === "save-text"){
+    console.log(info.selectionText);
+  }
 })
 
+
+const onCreated = () => {
+  if(browser.runtime.lastError) {
+    console.log("error creating item", browser.runtime.lastError);
+  } else {
+    console.log("item created successfully")
+  }
+}
+
+browser.menus.create({
+  id: "save-text",
+  title: "Highlight Text",
+  contexts: ["selection"],
+}, onCreated);
+
+
+browser.menus.create({
+  id: "green",
+  type: "radio",
+  title: "Green",
+  contexts: ["all"],
+  checked: false
+}, onCreated)
+
+browser.menus.create({
+  id: "yellow",
+  type: "radio",
+  title: "Yellow",
+  contexts: ["all"],
+  checked: false
+}, onCreated)
+
+
+browser.menus.create({
+  id: "blue",
+  type: "radio",
+  title: "Blue",
+  contexts: ["all"],
+  checked: false
+}, onCreated)
+
+browser.menus.create({
+  id: "red",
+  type: "radio",
+  title: "Magenta",
+  contexts: ["all"],
+  checked: false
+}, onCreated)
--- mark_my_words.js
+++ mark_my_words.js
@@ -9,3 +9,4 @@ document.addEventListener('mouseup', event => {
     //    console.log(exactText)        
     }  
 })
+
