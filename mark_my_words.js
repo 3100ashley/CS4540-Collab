000f9e7b52ffd780dfc60a84d35174e1ea89ce03
Ashley Manese <ashleymanese31@gmail.com>
2022-12-01 16:20:05 -0800

    able to change highlight color from menu selection

--- mark_my_words.js
+++ mark_my_words.js
@@ -1,10 +1,25 @@
 //highlight text
-    document.addEventListener('mouseup', event => {  
-        if(window.getSelection().toString().length){
-        let range = window.getSelection().getRangeAt(0);
-        let newNode = document.createElement('span');
-        newNode.style.backgroundColor = "rgb(250, 255, 176, .5)";
-        range.surroundContents(newNode);
-        browser.runtime.sendMessage("Marked Page", (response) => {console.log(response)});
-        }
-    })
+function highlightText(color){
+  if(window.getSelection().toString().length){
+    let range = window.getSelection().getRangeAt(0);
+    let newNode = document.createElement('span');
+    newNode.style.backgroundColor = color;
+    range.surroundContents(newNode)
+    browser.runtime.sendMessage("Marked Page", (response) => {console.log(response)}); 
+    } 
+ }
+
+browser.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(request)
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension");
+    if(request.color !== request.color){
+      document.removeEventListener('mouseup',highlightText(request.color))
+    }
+    document.addEventListener('mouseup',highlightText(request.color))
+  }
+);
+ 
+ 
